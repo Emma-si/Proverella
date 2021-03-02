5902aa2996afda30e563ff139d99bec8946dbe58
Emm Signorile <emma.signorile@hotmail.com>
2021-03-02 17:40:50 +0100

    aggiunta dati

---
+++ castellani.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  2 17:26:28 2021
+
+@author: emma
+"""
+
+import pandas as pd
+#import utils    # mie funzioni
+
+# Pandas legge i file
+centroidi_df = pd.read_csv("data/prova_centroidi.csv")
+
+print(centroidi_df)
+
+
+
+
